9298b70801fe2edca5e6c1c1d7ad897087f98092
NagaiKoki <kokinagai10+1@gmail.com>
2021-05-09 11:41:03 +0900

    FIX

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,18 @@ import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
-      <div>
+      <div className="Container">
         <Header />
         <Component {...pageProps} />
       </div>
+      <style jsx>{`
+        .Container {
+          margin-right: auto;
+          margin-left: auto;
+          max-width: 42rem;
+          padding: 2.625rem 1.3125rem;
+        }
+      `}</style>
     </ChakraProvider>
   );
 };
--- src/components/moleclues/Header.tsx
+++ src/components/moleclues/Header.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Heading, Switch } from "@chakra-ui/react";
 
 export const Header: React.VFC = () => {
+  const { toggleColorMode } = useColorMode();
+
   return (
     <header>
-      <h1>Koky Articles</h1>
+      <Heading as="h1" size="xl" colorScheme="whiteAlpha">
+        Koky Articles
+      </Heading>
+      <Switch size="lg" onChange={toggleColorMode} />
+      <style jsx>{`
+        header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+      `}</style>
     </header>
   );
 };
