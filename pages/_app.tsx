7ca2408d4c7e6587ec2031164b67bd533246f1a9
NagaiKoki <kokinagai10+1@gmail.com>
2021-05-09 11:17:19 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { Header } from "components/moleclues/Header";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider>
+      <div>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
+  );
+};
 
 export default MyApp;
---
+++ src/components/moleclues/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header: React.VFC = () => {
+  return (
+    <header>
+      <h1>Koky Articles</h1>
+    </header>
+  );
+};
