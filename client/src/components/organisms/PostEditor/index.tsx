9c1c84a206598166b22edb1cf4be483946ee59b6
NagaiKoki <kokinagai10+1@gmail.com>
2021-05-23 21:00:36 +0900

    fix

--- client/src/components/organisms/PostEditor/index.tsx
+++ client/src/components/organisms/PostEditor/index.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
+import { PostEditorTitleInput } from "./PostEditorTitleInput";
 import { PostEditorTextarea } from "./PostEditorTextarea";
 import { PostEditorPreview } from "./PostEditorPreview";
 import { COLORS } from "styles/index";
 
 export const PostEditor = () => {
+  const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
   return (
-    <div className="Wrapper">
-      <div className="Textarea__Wrapper">
-        <PostEditorTextarea value={content} onChange={setContent} />
+    <div className="Container">
+      <div className="Title__Wrapper">
+        <PostEditorTitleInput title={title} onChange={setTitle} />
       </div>
-      <PostEditorPreview content={content} />
+      <div className="Wrapper">
+        <div className="Textarea__Wrapper">
+          <PostEditorTextarea value={content} onChange={setContent} />
+        </div>
+        <PostEditorPreview content={content} />
+      </div>
       <style jsx>{`
         .Wrapper {
           display: flex;
+        }
+        .Title__Wrapper {
+          width: 95%;
+          margin: 20px auto;
         }
         .Textarea__Wrapper {
           width: 50%;
--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -2,4 +2,5 @@ export const COLORS = {
   TEXT_COLOR: "#050505",
   WHITE: "#FFF",
   GRAY_COLOR_1: "#ebebeb",
+  GRAY_COLOR_2: "#bcbcbc",
 };
