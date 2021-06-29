be4ec57cb34d769711abf32d2153aa5897d63258
koki <kokinagai10+1@gmail.com>
2021-06-29 17:59:09 +0900

    fix

---
+++ client/src/hooks/useCreatePost.ts
@@ -0,0 +1,13 @@
+import { mutate } from 'swr'
+
+import { createPosts } from "../lib/apis/createPost"
+import { RequestPostType } from '../types/post'
+import { SWR_CREATE_POST_KEY } from '../constants/swrKey'
+
+export const useGetPosts = async (args: RequestPostType) => {
+  try {
+    await mutate(SWR_CREATE_POST_KEY, createPosts(args))
+  } catch(e) {
+    return { payload: 'error' }
+  }
+}
