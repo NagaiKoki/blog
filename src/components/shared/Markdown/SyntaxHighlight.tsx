0c03411252969341af54d47e3fbc2d30a155200d
NagaiKoki <kokinagai10+1@gmail.com>
2024-04-01 01:05:51 +0900

    fix: tsc

--- src/components/shared/Markdown/SyntaxHighlight.tsx
+++ src/components/shared/Markdown/SyntaxHighlight.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-const CodeBlock = ({ language, value }) => {
+const CodeBlock = ({
+  language,
+  value
+}: {
+  language: string;
+  value: string;
+}) => {
   let newCode = value;
   return (
     <SyntaxHighlighter
--- src/pages/posts/new.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { EditorContainer } from '@components/domains/Post/Editor/EditorContainer';
-
-const PostNew = () => {
-  return <EditorContainer />;
-};
-
-export default PostNew;
