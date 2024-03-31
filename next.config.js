37b59a1e9641bcf0177bff31f4af7d19edcba868
NagaiKoki <kokinagai10+1@gmail.com>
2024-04-01 01:21:39 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-const moduleExports = {
-  // Your existing module.exports
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export'
 };
-
-const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  environment: process.env.NEXT_PUBLIC_ENV
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+/* module.exports = nextConfig*/
+module.exports = nextConfig;
