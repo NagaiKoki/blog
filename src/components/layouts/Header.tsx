5257eb505b739f96d9e7e53a85bad37dc0f6d1cb
NagaiKoki <kokinagai10+1@gmail.com>
2024-04-16 19:02:04 +0900

    feat: switch lang

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,36 @@
 import { useColorMode } from '@chakra-ui/color-mode';
-import { Heading, Switch } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { TITLE } from '@/constants/title';
+import { useLang } from '@/stores/useLang';
+import { Lang } from '@/types/lang';
+
+const jaPattern = /\/ja\/(\d+)/;
+const enPattern = /\/en\/(\d+)/;
 
 export const Header: React.VFC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { pathname } = useRouter();
+  const { pathname, replace } = useRouter();
+  const { lang: selectedLang, setLang } = useLang();
+
+  const handleOnChangeLang = useCallback(
+    async (lang: Lang) => {
+      setLang(lang);
+      if (lang === 'ja') {
+        if (enPattern.test(pathname) && lang === 'ja') {
+          await replace(pathname.replace(enPattern, '/ja/$1'));
+        }
+      } else {
+        if (jaPattern.test(pathname) && lang === 'en') {
+          await replace(pathname.replace(jaPattern, '/en/$1'));
+        }
+      }
+    },
+    [pathname, replace, setLang]
+  );
 
   return (
     <header>
@@ -25,18 +47,43 @@ export const Header: React.VFC = () => {
           {TITLE}
         </Heading>
       </Link>
-      <Switch
-        size="lg"
-        onChange={toggleColorMode}
-        colorScheme="yellow"
-        title="dd"
-        display="flex"
-        alignItems="center"
-      >
-        <Heading as="span" size="lg" paddingLeft="5px">
+      <Flex>
+        <Flex alignItems="center" marginRight="24px" gap={2}>
+          <Button
+            variant="ghost"
+            padding="2px 16px"
+            height="32px"
+            borderRadius="full"
+            fontSize="sm"
+            background={selectedLang === 'en' ? 'orange' : ''}
+            onClick={() => handleOnChangeLang('en')}
+          >
+            En
+          </Button>
+          <Text>/</Text>
+          <Button
+            variant="ghost"
+            padding="2px 16px"
+            borderRadius="full"
+            fontSize="sm"
+            height="32px"
+            background={selectedLang === 'ja' ? 'orange' : ''}
+            onClick={() => handleOnChangeLang('ja')}
+          >
+            Ja
+          </Button>
+        </Flex>
+        <Button
+          as="span"
+          fontSize="30px"
+          variant="goast"
+          cursor="pointer"
+          paddingLeft="5px"
+          onClick={toggleColorMode}
+        >
           {colorMode === 'dark' ? '🌘' : '🌞'}
-        </Heading>
-      </Switch>
+        </Button>
+      </Flex>
       <style jsx>{`
         header {
           display: flex;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link';
 import { articleConfig } from '@/articles/config';
 import { Aside } from '@/components/layouts/Aside';
 import { Seo } from '@/components/layouts/Seo';
+import { useLang } from '@/stores/useLang';
 
 export default function Home() {
+  const { lang } = useLang();
+
   return (
     <main>
       <Seo />
@@ -19,14 +22,14 @@ export default function Home() {
           )
           .map(([key, value]) => {
             return (
-              <Link key={value.createdAt} href={`/ja/${key}`}>
+              <Link key={value.createdAt} href={`/${lang}/${key}`}>
                 <Text
                   as="span"
                   fontSize="16px"
                   textDecoration="underline"
                   whiteSpace="pre-wrap"
                 >
-                  {`${value.createdAt}${'  -  '}${value.title.ja}`}
+                  {`${value.createdAt}${'  -  '}${value.title[lang]}`}
                 </Text>
               </Link>
             );
